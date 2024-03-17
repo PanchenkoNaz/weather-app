0a7cd7be7a9075cc5b2e3df7631c10e3c8d2d93f
Nazar Panchenko <120579167+PanchenkoNaz@users.noreply.github.com>
2024-03-17 18:39:17 +0100

    api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -26,5 +26,17 @@ export const url = {
     },
     forecast(lat, lon) {
         return 'https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric'
+    },
+    airPollution(lat, lon) {
+        return 'http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}'
+    },
+    reverseGeo(lat, lon) {
+        return 'http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5'
+    },
+    /**
+     * @param {string} query 
+     */
+    geo(query) {
+        return 'http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5'
     }
 }
