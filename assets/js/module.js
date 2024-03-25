f7d6cb682b623f25bd68ee2686c63c94128adf63
Nazar Panchenko <120579167+PanchenkoNaz@users.noreply.github.com>
2024-03-25 13:02:51 +0100

    module.js

--- assets/js/module.js
+++ assets/js/module.js
@@ -62,3 +62,51 @@ export const getTime = function (timeUnix, timezone) {
 
     return '${hours % 12 || 12}:${minutes} ${period}';
 }
+
+/**
+ * 
+ * @param {number} timeUnix Unix date in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds 
+ * @returns {string} Time string. formate: "HH AM/PM"
+ */
+
+export const getHours = function (timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date.getUTCHours();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return '${hours % 12 || 12} ${period}';
+}
+
+/**
+ * 
+ * @param {number} mps Metter per seconds 
+ * @returns {number} Kilometer per hours
+ */
+export const mps_to_kmh = mps => {
+    const mph = mps * 3600;
+    return mph / 1000;
+}
+
+export const aqiText = {
+    1: {
+        level: "Good",
+        message: "Air quality is considered satisfactory, and air pollution poses little or no risk."
+    },
+    2: {
+        level: "Fair",
+        message: "Air quality is acceptable; however, for some pollutants there may be a moderate health concern  a very small number of people who are unusually sensitive to air pollution."
+    },
+    3: {
+        level: "Moderate",
+        message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+    },
+    4: {
+        level: "Poor",
+        message: "Everyone may begin to experience health effects; members of sensitive groups may expirience more serious health effects."
+    },
+    5: {
+        level: "Very Poor",
+        message: "Health warnings of emergency conditions. The entire population is more likely to be affected."
+    }
+}
