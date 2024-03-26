95393c69e37befe7702f35b0cfdc4f9efe7d75a8
Nazar Panchenko <120579167+PanchenkoNaz@users.noreply.github.com>
2024-03-26 15:22:02 +0100

    route.js

--- assets/js/route.js
+++ assets/js/route.js
@@ -4,3 +4,47 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict';
+
+import { updateWeather, error404 } from "./api.js";
+const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474" //London
+
+const currentLocation = function() {
+    window.navigator.geolocation.getCurrentPosition(res => {
+        const { latitude, longitude } = res.coords;
+
+        updateWeather('lat=${latitude}', )
+    });
+    
+}
+
+/**
+ * @param {string} query Searched query 
+ */
+const searchedLocation = query => updateWeather(...query.split("&"));
+// updateWeather("lat=51.5073219", "lon=-0.1276474")
+    
+    
+const routes = new Map([
+    ["/current-location", currentLocation],
+    ["/weather", searchedLocation]
+]);
+
+const checkHash = function () {
+    const requestURL = window.location.hash.slice(1);
+
+    const [route, query] = requestURL.includes ? requestURL.split("?") : [requestURL];
+
+    routes.get(route) ? routes.get(route)(query) : error404();
+}
+
+window.addEventListener("hashchange", checkHash);
+
+window.addEventListener("load", function () {
+    if (!this.window.location.hash) {
+        wimdow.location.hash = "#/current-location";
+    } else {
+        checkHash();
+    }
+});
